bc2761ff444371ca818126d8dbc3f3694669a20b
ThanhTam457 <108389068+ThanhTam457@users.noreply.github.com>
2023-03-15 00:44:52 +0700

    update
    dvd

--- src/Booking.js
+++ src/Booking.js
@@ -5,12 +5,12 @@ import './css/Booking.css'
 
 const Booking = () => {
     return ( 
-        <div className="booking">
+        <div className="booking my-3">
             <div className="container m-0">
                 <Row>
                     <Col lg="6" className="ps-5 pe-5 align-self-center">
-                        <div className='mt-2 p-1'>
-                            <iframe height={400}
+                        <div className='mt-0 p-0'>
+                            <iframe height={405}
                                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2692.6956572804993!2d106.68109885609414!3d10.762891104474834!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752f1c06f4e1dd%3A0x43900f1d4539a3d!2sUniversity%20of%20Science%20-%20VNUHCM!5e0!3m2!1sen!2s!4v1642059695858!5m2!1sen!2s">
                             </iframe>
                             <div className='location bg-location'>
@@ -22,7 +22,7 @@ const Booking = () => {
                     </Col>
                     
                     <Col lg="6" className="ps-5 pe-5">
-                        <form action="#" className='mt-2 p-1'>
+                        <form action="#" className='container m-0'>
                             <h3 className="text fw-light"><i className="fa-solid fa-phone"></i>Book your table here</h3>
                             <hr />
                             <hr />
