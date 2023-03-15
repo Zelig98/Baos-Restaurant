6146440e52a7e5c499dea7282ed14d32b9f4c655
Minh_Cuteee <nhatm490@gmail.com>
2023-03-15 20:07:18 +0700

    tạm thời UI cho menu order và sửa linh tinh
    - Sửa chính tả cho mainpage.
    - Thêm data cứng node js cho food để list ra tạm thời, (chưa code function filter cho food categories).
    - Xóa restrict mode trong index.js để use effect không bị chạy 2 lần mỗi khi render page

--- src/Menu.js
+++ src/Menu.js
@@ -1,7 +1,57 @@
+import { useEffect, useState } from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import "./css/Menu.css"
+import FoodList from "./FoodList";
+
 const Menu = () => {
+    const [foods, setFoods] = useState(null);
+    const [isPending, setPending] = useState(true);
+
+    useEffect(() => {
+        console.log("use effect ran");
+
+        fetch('http://localhost:8000/foods')
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            setPending(false);
+            setFoods(data);
+        });
+    }, []);
+
     return ( 
         <div className="Menu">
-            Menu page
+            <Container>
+                <Row>
+                    <Col md="3" className="d-flex menu-categories flex-md-column d-block">
+                        <button type="button" className="menu-categories-btn btn fs-4 fw-bold text-start">All</button>
+                        <button type="button" className="menu-categories-btn btn fs-4 fw-bold text-start">Main Courses</button>
+                        <button type="button" className="menu-categories-btn btn fs-4 fw-bold text-start">Deserts</button>
+                        <button type="button" className="menu-categories-btn btn fs-4 fw-bold text-start">Beverages</button>
+                    </Col>
+                    <div className="vertical-divider p-0 d-none d-md-block">
+                        <div className="vr opacity-100 p-0"></div>
+                    </div>
+
+                    <Col md="9" className="foods-area ps-4">
+                        <h3 className="font-milonga mt-3 mt-md-0">MENU</h3>
+                        <div className='divider-thin bg-dark mb-2'></div>
+                        <div className='divider-thin bg-dark mb-3'></div>
+
+                        {/* List food area */}
+                        {isPending && 
+                        <div className="text-center">
+                            <div className="spinner-border m-5" role="status" style={{width: "5rem", height: "5rem"}}>
+                                <span className="visually-hidden">Loading...</span>
+                            </div>
+                        </div>}
+                        {foods && <FoodList foods={foods}/>}                  
+                        {/* End of List food area */}
+                    </Col>
+
+                </Row>
+            </Container>
         </div>
      );
 }
