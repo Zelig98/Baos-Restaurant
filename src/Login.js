3d9ced713afe467c62859832ae03cc20d0b8d6f8
VuBinhGiaUy <giauy1309@gmail.com>
2023-04-01 09:29:19 +0700

    Minor fix (file and structure change)

--- src/Login.js
+++ src/Login.js
@@ -1,51 +1,14 @@
 import React, { useState } from "react";
+//Icon
 import emailIcon from "../img/email.svg";
 import passwordIcon from "../img/password.svg";
-import styles from "./SignUp.module.css";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { notify } from "./toast";
+//Style
+import styles from "./SignUp.css";
+//
 import { Link } from "react-router-dom";
-import axios from "axios";
 
 const Login = () => {
-  const [data, setData] = useState({
-    email: "",
-    password: "",
-  });
-
-  const [touched, setTouched] = useState({});
-
-  const chaeckData = (obj) => {
-    const { email, password } = obj;
-    const urlApi = `https://lightem.senatorhost.com/login-react/index.php?email=${email.toLowerCase()}&password=${password}`;
-    const api = axios
-      .get(urlApi)
-      .then((response) => response.data)
-      .then((data) => (data.ok ? notify("You login to your account successfully", "success") : notify("Your password or your email is wrong", "error")));
-    toast.promise(api, {
-      pending: "Loading your data...",
-      success: false,
-      error: "Something went wrong!",
-    });
-  };
-
-  const changeHandler = (event) => {
-    if (event.target.name === "IsAccepted") {
-      setData({ ...data, [event.target.name]: event.target.checked });
-    } else {
-      setData({ ...data, [event.target.name]: event.target.value });
-    }
-  };
-
-  const focusHandler = (event) => {
-    setTouched({ ...touched, [event.target.name]: true });
-  };
-
-  const submitHandler = (event) => {
-    event.preventDefault();
-    chaeckData(data);
-  };
+  
 
   return (
     <div className={styles.container}>
--- src/SignUp.js
+++ src/SignUp.js
@@ -1,79 +1,15 @@
 import React, { useEffect, useState } from "react";
 //Icon
-import userIcon from "../img/user.svg";
-import emailIcon from "../img/email.svg";
-import passwordIcon from "../img/password.svg";
-// Validate
-import { validate } from "./validate";
+import userIcon from "../images/icon/user.svg";
+import emailIcon from "../images/icon/email.svg";
+import passwordIcon from "../images/icon/password.svg";
 // Styles
-import styles from "./SignUp.module.css";
-import "react-toastify/dist/ReactToastify.css";
-// Toast
-import { ToastContainer, toast } from "react-toastify";
-import { notify } from "./toast";
+import styles from "./SignUp.css";
 //
 import { Link } from "react-router-dom";
-// Axios
-import axios from "axios";
 
 const SignUp = () => {
-  const [data, setData] = useState({
-    name: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
-    IsAccepted: false,
-  });
 
-  const [errors, setErrors] = useState({});
-  const [touched, setTouched] = useState({});
-
-  useEffect(() => {
-    setErrors(validate(data, "signUp"));
-  }, [data, touched]);
-
-  const changeHandler = (event) => {
-    if (event.target.name === "IsAccepted") {
-      setData({ ...data, [event.target.name]: event.target.checked });
-    } else {
-      setData({ ...data, [event.target.name]: event.target.value });
-    }
-  };
-
-  const focusHandler = (event) => {
-    setTouched({ ...touched, [event.target.name]: true });
-  };
-
-  const submitHandler = (event) => {
-    event.preventDefault();
-    if (!Object.keys(errors).length) {
-      // Pushing data to database usuing PHP script
-      const urlApi = `https://lightem.senatorhost.com/login-react/index.php?email=${data.email.toLowerCase()}&password=${data.password}&register=true`;
-      const pushData = async () => {
-        const responseA = axios.get(urlApi);
-        const response = await toast.promise(responseA, {
-          pending: "Check your data",
-          success: "Checked!",
-          error: "Something went wrong!",
-        });
-        if (response.data.ok) {
-          notify("You signed Up successfully", "success");
-        } else {
-          notify("You have already registered, log in to your account", "warning");
-        }
-      };
-      pushData();
-    } else {
-      notify("Please Check fileds again", "error");
-      setTouched({
-        name: true,
-        email: true,
-        password: true,
-        confirmPassword: true,
-        IsAccepted: false,
-      });
-    }
-  };
 
   return (
     <div className={styles.container}>
