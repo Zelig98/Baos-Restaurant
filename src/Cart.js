555bcbc0bfec2fec671ba1b1d41b1fe5918b4932
--unset <trieuhuy.la@gmail.com>
2023-04-03 14:31:14 +0700

    cart UI fix

--- src/Cart.js
+++ src/Cart.js
@@ -1,26 +1,29 @@
 import './css/Cart.css'
-import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
+import React, { useState, useContext } from 'react';
 import Offcanvas from 'react-bootstrap/Offcanvas';
+import { CartContext } from './include/CartContext';
 
 const Cart = () => {
+    const cart = useContext(CartContext)
+
     const [show, setShow] = useState(false);
 
     const handleShow = () => setShow(true);
     const handleClose = () => setShow(false);
 
+    const itemCount = cart.items.reduce((sum, product) => sum + product.quantity, 0);
+
     return (
         <>
-            <Button className='cart-toggle-btn' onClick={handleShow} variant='light'>
+            <a className='cart-toggle-btn' onClick={handleShow}>
                 <i className="fa-solid fa-cart-shopping ms-2 fs-1 me-md-5"></i>
-            </Button>
-            <Offcanvas show={show} onHide={handleClose}>
+            </a>
+            <Offcanvas placement="right" className="custom-cart" show={show} onHide={handleClose}>
                 <Offcanvas.Header closeButton>
                     <Offcanvas.Title>Order Cart</Offcanvas.Title>
                 </Offcanvas.Header>
                 <Offcanvas.Body>
-                    Some text as placeholder. In real life you can have the elements you
-                    have chosen. Like, text, images, lists, etc.
+                    {}
                 </Offcanvas.Body>
             </Offcanvas>
         </>
