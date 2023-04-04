6831a89254a9feb29e8145255001c0356b510922
Minh_Cuteee <nhatm490@gmail.com>
2023-04-04 12:48:08 +0700

    Update Cart.js
    bấm check out sẽ clear cart items

--- src/Cart.js
+++ src/Cart.js
@@ -38,6 +38,8 @@ const Cart = () => {
         console.log(foodCartSession);
         addToPayment();
         handleClose();
+        ReactSession.set('cart', [])
+        setFoodCartBody([]);
 
         alert("Foods is added to Payment");
     };
